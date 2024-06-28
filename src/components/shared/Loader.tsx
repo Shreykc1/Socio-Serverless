522bd6801bff169343de7a7a10f3a701dcf5ea5e
Shrey Chandpa <shreykc1@gmail.com>
2024-06-28 05:56:36 +0100

    first

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,16 @@
+
+
+const Loader = () => {
+  return (
+    <div className="flex-center w-full">
+        <img
+        alt="loader"
+        src="/assets/icons/loader.svg"
+        width={20}
+        height={20}
+        />
+    </div>
+  )
+}
+
+export default Loader
